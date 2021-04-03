ba449ce1a66f18aa43326e90513acd1620118b4a
ventura-wu <xlucs@qq.com>
2021-04-03 21:35:28 +0800

    [wuchao] #N/A insert swagger to project.

--- bigdata/account/src/main/java/com/wch/account/controller/UserController.java
+++ bigdata/account/src/main/java/com/wch/account/controller/UserController.java
@@ -3,8 +3,13 @@ package com.wch.account.controller;
 import com.wch.account.dto.UserDTO;
 import com.wch.account.mapper.ModelMapper;
 import com.wch.account.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -15,6 +20,9 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(path = {"/account/user"})
+@EnableSwagger2
+@Api(tags = {"用户信息-接口管理"})
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -23,6 +31,8 @@ public class UserController {
     private ModelMapper modelMapper;
 
     @GetMapping(value = "/{userId}")
+    @ApiOperation(value = "根据ID获取USER的信息")
+    @ApiImplicitParam(name = "userId",value = "用户ID",required = true,dataType = "Long",paramType = "path")
     public UserDTO getUser(@PathVariable("userId") Long userId) {
         return modelMapper.map(userService.findUserPoById(userId), UserDTO.class);
     }
--- bigdata/account/src/main/java/com/wch/account/dto/UserDTO.java
+++ bigdata/account/src/main/java/com/wch/account/dto/UserDTO.java
@@ -1,6 +1,10 @@
 package com.wch.account.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -9,24 +13,37 @@ import java.util.Date;
  * @date 2021/4/1
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
+    @ApiModelProperty(value = "用户ID")
     private Long userId;
 
+    @ApiModelProperty(value = "名称")
     private String userName;
 
+    @ApiModelProperty(value = "密码")
     private String passWord;
 
+    @ApiModelProperty(value = "性别")
     private String sex;
 
+    @ApiModelProperty(value = "出生日期")
     private Date birthday;
 
+    @ApiModelProperty(value = "电话号码")
     private String phone;
 
+    @ApiModelProperty(value = "创建者")
     private String createUser;
 
+    @ApiModelProperty(value = "创建日期")
     private Long createTime;
 
+    @ApiModelProperty(value = "最后修改用户")
     private String lastModifyUser;
 
+    @ApiModelProperty(value = "最后修改时间")
     private Long lastModifyTime;
 }
